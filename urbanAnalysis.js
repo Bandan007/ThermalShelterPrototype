5aabdc8a6b9fad988782a3f9f68e2c4bf9d738c7
Bandan007 <bandandeykarmakar@proton.me>
2026-09-09 19:33:57 +0530

    Update urbanAnalysis.js

--- urbanAnalysis.js
+++ urbanAnalysis.js
@@ -1332,7 +1332,7 @@ window.selectLocation =
         if (climateDisplay) {
 
             climateDisplay.innerText =
-                "Loading climate + surroundings... <br> <font size=1>this might take upto 3 minuites, please be patient</font>";
+                "Loading climate + surroundings... <br> <font size=0.5em>this might take upto 3 minuites, please be patient</font>";
         }
 
 
